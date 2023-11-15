d4bc988baa80b9a5cff3380d4402fb7ea9ce2f98
Sandro Gakharia <sandrogakharia@gmail.com>
2023-11-16 02:07:54 +0400

    Add my solution to all testcases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0) return "ERROR";
+  if (start !== +start || end !== +end) return "ERROR";
 
+  let sum = 0;
+  if (start < end) {
+    for (let i = start; i <= end; i++) sum += i;
+  } else {
+    for (let i = end; i <= start; i++) sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
