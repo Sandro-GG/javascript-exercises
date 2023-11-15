7280e5d7040a4f8884c7f23bfe984962223f0f25
Sandro Gakharia <sandrogakharia@gmail.com>
2023-11-16 01:58:02 +0400

    Add my solution to all testcases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,32 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+  let howMany = arguments.length - 1;
+  console.log(howMany);
+  if (howMany === 1) {
+    for (let i of array) {
+      if (array[i - 1] === arguments[1]) {
+        array.splice(i - 1, 1);
+        break;
+      }
+    }
+  } else {
+    let i = 0;
+    while (i < howMany) {
+      for (let j = 0; j < array.length; j++) {
+        if (array[j] === arguments[i + 1]) { 
+          array.splice(j, 1);
+          break;
+        }
+      }
+      i++;
+    }
+  }
+  return array;
 };
+
+// way simpler solution:
+// var removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
