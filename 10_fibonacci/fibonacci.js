cc567e1ab9a9929a1620982b13f4a7efeeb2af0d
Sandro Gakharia <sandrogakharia@gmail.com>
2024-01-08 01:31:01 +0400

    Add solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
+  let arr = [];
+  arr[0] = 1;
+  arr[1] = 1;
+  for (let i = 1; i < 30; i++) {
+    arr.push(arr[i - 1] + arr[i]);
+  }
+  return arr[n - 1];
 };
 
 // Do not edit below this line
